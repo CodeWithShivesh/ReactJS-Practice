6d01555e17875b0ada36695d21ccc6b8f78f3996
CodeWIthShivesh <shiveshthestudytop@gmail.com>
2024-09-06 14:26:31 +0530

    bharat Clock

---
+++ Bharat-Clock/src/App.jsx
@@ -0,0 +1,16 @@
+import AppName from "./components/AppName";
+import AppClock from "./Components/AppClock";
+import AppHeading from "./Components/AppHeading";
+import "./App.css";
+
+function App() {
+  return (
+    <center>
+      <AppName></AppName>
+      <AppHeading></AppHeading>
+      <AppClock></AppClock>
+    </center>
+  );
+}
+
+export default App;
