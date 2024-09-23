24441186caa445ccd2b6f5d1288b06317d646c1a
CodeWIthShivesh <shiveshthestudytop@gmail.com>
2024-09-23 12:38:35 +0530

    Started MyPortfolio

---
+++ MyProtfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="p-4 bg-gray-700 text-yellow-100 text-2xl">
+        My Mega Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
